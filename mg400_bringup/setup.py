afef944713eee83a8828d126e3d260ae8f4165e9
m12watanabe1a <m12watanabe1a@gmail.com>
2022-02-24 00:33:09 +0900

    generate mg400 basic packages

---
+++ mg400_bringup/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'mg400_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='m12watanabe1a',
+    maintainer_email='m12watanabe1a@gmail.com',
+    description='Dobot MG400 launch file associated packages.',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
