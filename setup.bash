451f7bf6fd2c4308c33125ebe3bcae54d9c4ed78
m12watanabe1a <m12watanabe1a@gmail.com>
2022-06-07 15:34:19 +0900

    fix ci

--- setup.bash
+++ setup.bash
@@ -2,14 +2,19 @@
 
 THIS_FILE=$BASH_SOURCE
 THIS_PROJECT_ROOT=$(realpath $(dirname $(realpath $THIS_FILE)))
-
-# Install python packages
-python3 -m pip install -U -r $THIS_PROJECT_ROOT/requirements.txt
+THIS_REPOSITORY_NAME="mg400"
 
 # Install ROS2 dependency
+## From git repos
+colcon_ws=${THIS_PROJECT_ROOT}/../../
+cd ${colcon_ws}/src
+vcs import --recursive <${THIS_PROJECT_ROOT}/${THIS_REPOSITORY_NAME}.repos
 unset colcon_ws
+
+## From apt repositories
 rosdep update
 rosdep install -r -y -i --from-paths ${THIS_PROJECT_ROOT} --rosdistro $ROS_DISTRO
 
 unset THIS_FILE
 unset THIS_PROJECT_ROOT
+
