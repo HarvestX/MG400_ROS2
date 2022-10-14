7cfcb4300e64eb25ae7bc7cc6eb93b97956abd1c
WATANABE Aoi <40206149+m12watanabe1a@users.noreply.github.com>
2022-10-14 12:06:27 +0900

    fix service msg (#102)
    * fix service msg
    
    * fix service node
    
    * fix sample script

--- sample.bash
+++ sample.bash
@@ -9,20 +9,13 @@ ros2 service call /mg400/enable_robot mg400_msgs/srv/EnableRobot
 # Move
 ## Roll
 ros2 service call /mg400/mov_j mg400_msgs/srv/MovJ \
-  "{x: 0.34, y: 0.0, z: 0.0, rx: 0.0, ry: 0.0, rz: 0.0}"
+  "{x: 0.34, y: 0.0, z: 0.0, r: 0.0}"
 ros2 service call /mg400/mov_j mg400_msgs/srv/MovJ \
-  "{x: 0.0, y: -0.34, z: 0.0, rx: 0.0, ry: 0.0, rz: 0.0}"
+  "{x: 0.0, y: -0.34, z: 0.0, r: 1.57}"
 ros2 service call /mg400/mov_j mg400_msgs/srv/MovJ \
-  "{x: 0.34, y: 0.0, z: 0.0, rx: 0.0, ry: 0.0, rz: 0.0}"
+  "{x: 0.34, y: 0.0, z: 0.0, r: 0.0}"
 
-ros2 service call /mg400/mov_l mg400_msgs/srv/MovL \
-  "{x: 0.2, y: 0.05, z: 0.0, rx: 0.0, ry: 0.0, rz: 0.0}"
-ros2 service call /mg400/mov_l mg400_msgs/srv/MovL \
-  "{x: 0.4, y: -0.05, z: 0.0, rx: 0.0, ry: 0.0, rz: 0.0}"
-ros2 service call /mg400/mov_l mg400_msgs/srv/MovL \
-  "{x: 0.34, y: 0.0, z: 0.0, rx: 0.0, ry: 0.0, rz: 0.0}"
-
-sleep 1
+sleep 5
 
 # Turn off
 ros2 service call /mg400/disable_robot mg400_msgs/srv/DisableRobot
