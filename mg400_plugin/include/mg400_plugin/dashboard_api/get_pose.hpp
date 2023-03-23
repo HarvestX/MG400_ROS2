8f6a0d0139e8552282d6e0f8132eb9b2cca2f2a2
Mt-is-at-O <mountain.inside1310@gmail.com>
2023-03-23 15:37:31 +0900

    add GetPose

---
+++ mg400_plugin/include/mg400_plugin/dashboard_api/get_pose.hpp
@@ -0,0 +1,40 @@
+// Copyright 2022 HarvestX Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <mg400_plugin_base/api_plugin_base.hpp>
+#include <mg400_msgs/srv/get_pose.hpp>
+
+namespace mg400_plugin
+{
+class GetPose final : public mg400_plugin_base::DashboardApiPluginBase
+{
+public:
+  using ServiceT = mg400_msgs::srv::GetPose;
+
+private:
+  rclcpp::Service<ServiceT>::SharedPtr srv_;
+
+public:
+  void configure(
+    const mg400_interface::DashboardCommander::SharedPtr,
+    const rclcpp::Node::SharedPtr,
+    const mg400_interface::RealtimeFeedbackTcpInterface::SharedPtr) override;
+
+private:
+  void onServiceCall(
+    const ServiceT::Request::SharedPtr, ServiceT::Response::SharedPtr);
+};
+}  // mg400_plugin
---
+++ mg400_plugin/src/dashboard_api/get_pose.cpp
@@ -0,0 +1,58 @@
+// Copyright 2022 HarvestX Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "mg400_plugin/dashboard_api/get_pose.hpp"
+
+namespace mg400_plugin
+{
+
+void GetPose::configure(
+  const mg400_interface::DashboardCommander::SharedPtr commander,
+  const rclcpp::Node::SharedPtr node,
+  const mg400_interface::RealtimeFeedbackTcpInterface::SharedPtr rt_if)
+{
+  if (!this->configure_base(commander, node, rt_if)) {
+    return;
+  }
+
+  using namespace std::placeholders;  // NOLINT
+  this->srv_ = node->create_service<ServiceT>(
+    "get_pose",
+    std::bind(&GetPose::onServiceCall, this, _1, _2));
+}
+
+void GetPose::onServiceCall(
+  const ServiceT::Request::SharedPtr,
+  ServiceT::Response::SharedPtr res)
+{
+  try {
+    auto poses = this->commander_->getPose();
+    res->pose1 = poses[0];
+    res->pose2 = poses[1];
+    res->pose3 = poses[2];
+    res->pose4 = poses[3];
+    res->pose5 = poses[4];
+    res->pose6 = poses[5];
+  } catch (const std::runtime_error & ex) {
+    RCLCPP_ERROR(this->base_node_->get_logger(), ex.what());
+  } catch (...) {
+    RCLCPP_ERROR(this->base_node_->get_logger(), "Interface Error");
+  }
+}
+}  // namespace mg400_plugin
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(
+  mg400_plugin::GetPose,
+  mg400_plugin_base::DashboardApiPluginBase)
