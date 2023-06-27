7aff4d666477f6e3c5ea6e7be69f255cc285abdd
ynyBonfennil <veloute.de.jonquille@gmail.com>
2023-06-27 16:34:34 +0900

    replace with lifecycle node

--- mg400_node/include/mg400_node/mg400_node.hpp
+++ mg400_node/include/mg400_node/mg400_node.hpp
@@ -25,11 +25,15 @@
 #include <mg400_plugin_base/api_loader_base.hpp>
 #include <mg400_plugin_base/api_plugin_base.hpp>
 #include <pluginlib/class_loader.hpp>
+#include <rclcpp_lifecycle/lifecycle_node.hpp>
 #include <rclcpp/rclcpp.hpp>
 
 namespace mg400_node
 {
-class MG400Node : public rclcpp::Node
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+using State = rclcpp_lifecycle::State;
+
+class MG400Node : public rclcpp_lifecycle::LifecycleNode
 {
 private:
   const std::vector<std::string> default_dashboard_api_plugins_ = {
@@ -47,20 +51,18 @@ private:
     "mg400_plugin::MovJ",
     "mg400_plugin::MovL"
   };
+
+  std::string ip_address_;
   mg400_interface::MG400Interface::SharedPtr interface_;
-
-  mg400_plugin_base::DashboardApiLoader::SharedPtr
-    dashboard_api_loader_;
-
-  mg400_plugin_base::MotionApiLoader::SharedPtr
-    motion_api_loader_;
+  mg400_plugin_base::DashboardApiLoader::SharedPtr dashboard_api_loader_;
+  mg400_plugin_base::MotionApiLoader::SharedPtr motion_api_loader_;
 
   rclcpp::TimerBase::SharedPtr init_timer_;
   rclcpp::TimerBase::SharedPtr joint_state_timer_;
   rclcpp::TimerBase::SharedPtr robot_mode_timer_;
   rclcpp::TimerBase::SharedPtr error_timer_;
   rclcpp::TimerBase::SharedPtr interface_check_timer_;
-  rclcpp::TimerBase::SharedPtr reconnect_timer_;
+  rclcpp::TimerBase::SharedPtr connect_timer_;
 
   rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_pub_;
   rclcpp::Publisher<mg400_msgs::msg::RobotMode>::SharedPtr robot_mode_pub_;
@@ -79,6 +81,10 @@ public:
   void onInterfaceCheckTimer();
 
 private:
+  CallbackReturn on_configure(const rclcpp_lifecycle::State &) override;
+  CallbackReturn on_activate(const rclcpp_lifecycle::State &) override;
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State &) override;
+
   void runTimer();
   void cancelTimer();
 };
