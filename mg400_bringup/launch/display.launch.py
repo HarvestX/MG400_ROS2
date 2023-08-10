8ca193af9960f765873171f0394705d807534f29
MrBearing <takumi1988okamoto@gmail.com>
2023-08-10 23:25:24 +0900

    update mesh file, launch files and urdf

--- mg400_bringup/launch/display.launch.py
+++ mg400_bringup/launch/display.launch.py
@@ -15,45 +15,67 @@
 
 from pathlib import Path
 from typing import Dict
+from typing import List
+from typing import Tuple
 
 from ament_index_python import get_package_share_path
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.actions import Shutdown
 from launch.substitutions import Command
 from launch.substitutions import FindExecutable
+from launch.substitutions import LaunchConfiguration
 from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import TextSubstitution
 from launch_ros.actions import Node
 
 
-def _load_robot_description(filepath: Path) -> Dict:
+def _load_robot_description(
+        xacro_filepath: Path, xacro_options: List[Tuple] = False) -> Dict:
     """Load robot description."""
-    if 'xacro' in filepath.suffix:
-        robot_description_content = Command(
-            [
-                PathJoinSubstitution([FindExecutable(name='xacro')]),
-                ' ',
-                str(filepath)
-            ],
-        )
+    if 'xacro' in str(xacro_filepath):
+        params = []
+        if xacro_options:
+            for xacro_option in xacro_options:
+                params.append(' {}:='.format(
+                    xacro_option[0]))
+                params.append(xacro_option[1])
+        command = [
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
+            str(xacro_filepath)]
+        robot_description_content = Command(command=(command + params))
     else:
         try:
-            with open(str(filepath), 'r') as file:
+            with open(str(xacro_filepath), 'r') as file:
                 robot_description_content = file
         except EnvironmentError:
             exit(1)
+
     return {'robot_description': robot_description_content}
 
 
 def generate_launch_description():
     """Launch rviz display."""
+    arg_workspace_visible = DeclareLaunchArgument(
+        'workspace_visible',
+        default_value=TextSubstitution(text='False'),
+        description='true : MG400 workspace is visible in rviz')
+
+    workspace_visible = LaunchConfiguration(
+        'workspace_visible', default='False')
+    xacro_filepath_ = get_package_share_path('mg400_description') / 'urdf' / 'mg400.urdf.xacro'
+    robot_description = _load_robot_description(
+        xacro_filepath=xacro_filepath_,
+        xacro_options={
+            'workspace_visible': workspace_visible,
+        }.items())
+
     rsp_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='log',
-        parameters=[
-            _load_robot_description(
-                get_package_share_path('mg400_description') /
-                'urdf' / 'mg400.urdf.xacro')])
+        parameters=[robot_description])
     jsp_node = Node(
         package='mg400_node',
         executable='joint_state_publisher_gui',
@@ -71,6 +93,7 @@ def generate_launch_description():
         ])
 
     ld = LaunchDescription()
+    ld.add_action(arg_workspace_visible)
     ld.add_action(rsp_node)
     ld.add_action(jsp_node)
     ld.add_action(rviz_node)
